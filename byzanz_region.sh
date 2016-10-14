da4adb35c13ac72e1509f291548b1dbcf88e1cf1
Dariush vesal <dariush.vesal@gmail.com>
2016-10-14 18:55:11 +0330

    Inital commit 23 Mehr 1395 Saat 18 o 55 Asr ;)

---
+++ byzanz_region.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Delay before starting
+DELAY=3
+
+DIRECTORY="recored"
+TIME=$(date +"%Y-%m-%d_%H%M%S")
+
+if [ ! -d "$DIRECTORY" ]; then
+  mkdir -p "$DIRECTORY"
+fi
+
+PATHX=`realpath $DIRECTORY`
+FILE_NAME="$PATHX/recored-$TIME.gif"
+
+
+
+# Sound notification to let one know when recording is about to start (and ends)
+beep() {
+  paplay /usr/share/sounds/KDE-Im-Irc-Event.ogg &
+}
+
+DEFDUR=10
+
+# Custom recording duration as set by user
+USERDUR=$(gdialog --title "Duration?" --inputbox "Please enter the screencast duration in seconds" 200 100 2>&1)
+
+# Duration and output file
+if [ $USERDUR -gt 0 ]; then
+  Duration=$USERDUR
+else
+  Duration=$DEFDUR
+fi
+
+
+
+D="--duration=$Duration $FILE_NAME"
+
+
+# xrectsel from https://github.com/lolilolicon/xrectsel
+ARGUMENTS=$(xrectsel "--x=%x --y=%y --width=%w --height=%h") || exit -1
+
+echo Delaying $DELAY seconds. After that, byzanz will start
+for (( i=$DELAY; i>0; --i )) ; do
+  echo $i
+  sleep 1
+done
+beep
+byzanz-record --verbose --delay=0 ${ARGUMENTS} $D
+beep
+xdg-open "$DIRECTORY"
